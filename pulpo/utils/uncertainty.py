6ae1cfdaac38b6322f69caa68a564c76338292f9
flechtenberg <fabian.lechtenberg@chem.ethz.ch>
2025-04-12 06:33:41 +0200

    placeholder for gsa

--- pulpo/utils/uncertainty.py
+++ pulpo/utils/uncertainty.py
@@ -1,3 +1,5 @@
+from pulpo.utils import saver
+
 def solve_model_MC(worker, n_it=100, gams_path=False, solver_name=None, options=None):
     """
     Perform very simple Monte Carlo (MC) simulations to solve a model multiple times, each time sampling A, B, and Q again.
@@ -20,3 +22,10 @@ def solve_model_MC(worker, n_it=100, gams_path=False, solver_name=None, options=
         worker.solve(gams_path, solver_name=solver_name, options=options)
         results.append(worker.instance.OBJ())
     return results
+
+def run_gsa(worker):
+    1+1
+
+
+
+
